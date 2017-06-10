0f977b0b29f0e4a46133b8afd426c56daa8014b4
ebridge2 <ebridge2@jhu.edu>
2017-06-10 09:47:35 -0400

    updated beta estimator function

--- R/beta_estimator.R
+++ R/beta_estimator.R
@@ -12,8 +12,8 @@
 #'
 beta_estimator <- function(sample) {
   me_samp <- mean(sample)
-  sig_samp <- std(sample)
+  sig_samp <- sd(sample)
   alpha <- ((1 - me_samp)/sig_samp^2 - 1/me_samp)*me_samp^2
-  beta <- sig_samp*(1/me_samp - 1)
+  beta <- alpha*(1/me_samp - 1)
   return(list(alpha=alpha, beta=beta))
 }
