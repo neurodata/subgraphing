2b93eafe587bee6cc9cb3f726999c825812dc9e2
Vikram Chandrashekhar <vchandr6@jhu.edu>
2019-03-08 10:44:53 -0500

    Revert "Revert "updated description""
    This reverts commit 85bda9fcc3268a30dbc62fc651170c2612c8918c.

--- R/subgraph_classifier.R
+++ R/subgraph_classifier.R
@@ -3,8 +3,6 @@
 #' \code{sg.bern.subgraph_classifier} classifies arbitrary data given the estimators of a subgraph.
 #'
 #' @param test the test array to generate labels for.
-#'     - if test is a list, then it should have s elements of dimensions
-#'         [n x n].
 #'    - if test is an array, then it should be of dimensions [n x n x s].
 #' @param p [n x n x c] the probability per edge of being connected per class for c classes.
 #' @param pi [c] the pi vector probability of each class occuring.
@@ -19,20 +17,22 @@ sg.bern.subgraph_classifier <- function(test, edges, p, pi, classes) {
   s <- dim(test)[3]
   c <- dim(p)[3]
   e <- length(edges)
-
+  # convert to logarithmic in case ps are small
+  pi <- log(pi)
+  p <- log(p)
   hyp <- array(NaN, dim=c(s, e, c))
   for (k in 1:c) {
-    pclass <- p[,,k]  # current probability matrix
+    pclass <- p[,,k]  # current probability matrix in log-form
     for (i in 1:s) {
       csamp <- test[,,i]  # current sample
       for (j in 1:e) {  # iterate over subgraph edges
         eix <- edges[j]  # id of the edge
-        hyp[i, j, k] <- pclass[eix]^csamp[eix]*(1 - pclass[eix])^(1 - csamp[eix])*pi[k]
+        hyp[i, j, k] <- pclass[eix]*csamp[eix] + (1 - pclass[eix])*(1 - csamp[eix]) + pi[k]
       }
     }
   }
 
-  h <- apply(hyp, c(1, 3), prod)  # product over the edges in the subgraph, the 3rd dimension
+  h <- apply(hyp, c(1, 3), sum)  # product over the edges in the subgraph, the 3rd dimension
 
   Yhat <- apply(h, c(1), FUN = function(x) {
     classes[sort(x, decreasing=TRUE, index.return=TRUE)$ix[1]]
